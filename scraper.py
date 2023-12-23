7ce2821edabd783b7eea4de5bf9fa0c39710e82b
ekolvah <43490155+ekolvah@users.noreply.github.com>
2023-12-23 17:31:46 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -74,13 +74,10 @@ def run_kinozal_scrapper():
   new_movies = kinozal_top_movies.merge(notified_movies, on='films', how='outer', indicator=True)
   new_movies = new_movies[new_movies['_merge'] == 'left_only']
   new_movies = new_movies.drop('_merge', axis=1)
+  notified_movies = pd.concat([notified_movies['films'], new_movies['films']])
   
   send_message_with_new_movies(new_movies)
-
-  #notified_movies = pd.concat([notified_movies['films'], new_movies['films']])
-  
-  save_notified_movies(notified_movies_worksheet, new_movies)
-  
+  save_notified_movies(notified_movies_worksheet, notified_movies)
   save_kinozal_top_movies(kinozal_top_movies_worksheet, kinozal_top_movies)
   
 run_kinozal_scrapper()
